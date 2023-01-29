a5cd21aedc8ec13709968be5409cd404ea8d3cd2
Heydenos <hitpark667@gmail.com>
2023-01-30 00:54:59 +0800

    add new post

--- blog.py
+++ blog.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect, url_for
 import sqlite3
 
 app = Flask(__name__)
+app.config['SECRET_KEY']="blog123"
 
 def get_db_conn():
     conn = sqlite3.connect("database.db")
@@ -16,12 +17,26 @@ def get_post(post_id):
 @app.route("/")
 def index():
     conn = get_db_conn()
-    posts = conn.execute("select * from posts").fetchall()
+    posts = conn.execute("select * from posts order by created desc").fetchall()
     return render_template("index.html", posts = posts)
 
 @app.route("/posts/new", methods=("GET", "POST"))
 def new():
-    print("success")
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        if not title:
+            flash("标题不能为空")
+        elif not content:
+            flash("内容不能为空")
+        else:
+            conn = get_db_conn()
+            conn.execute('INSERT INTO posts(title, content) values(?, ?)', (title, content))
+            conn.commit()
+            conn.close()
+            flash("提交成功")
+            return redirect(url_for('index'))
+            
     return render_template("new.html")
 
 @app.route("/posts/<int:post_id>")
