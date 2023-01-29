cad3c55a318c05fcbd17f424a32fb7ceec01ad33
Heydenos <hitpark667@gmail.com>
2023-01-29 23:44:47 +0800

    add post's page

--- blog.py
+++ blog.py
@@ -8,6 +8,11 @@ def get_db_conn():
     conn.row_factory = sqlite3.Row
     return conn
 
+def get_post(post_id):
+    conn = get_db_conn()
+    post = conn.execute("select * from posts where id = ?", (post_id,)).fetchone()
+    return post
+
 @app.route("/")
 def index():
     conn = get_db_conn()
@@ -18,3 +23,11 @@ def index():
 def new():
     print("success")
     return render_template("new.html")
+
+@app.route("/posts/<int:post_id>")
+def post(post_id):
+    post = get_post(post_id)
+    return render_template("post.html", post=post)
+
+if __name__=="__main__":
+    app.run(debug=True)
